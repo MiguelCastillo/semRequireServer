bcfcd401357e55f24caaa2aa5dc5aa9c988c4561
Miguel Castillo <manchagnu@gmail.com>
2014-04-12 01:05:39 -0400

    Added ability to read/update/delete specific and all items

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var mixin   = require("utils/mixin");
 var cors    = require("utils/cors");
 
 var app      = express();
-var messages = [];
+
+var _id = 0;
+var messages = {};
 
 
 // Support JSON bodies
@@ -13,18 +15,69 @@ app.use(express.json());
 cors(app);
 
 
+
+app.get("/messages", function(req, res) {
+  var _messages = [];
+  for(var i in messages) {
+    _messages.push(messages[i]);
+  }
+  res.send(_messages);
+});
+
+
+app.get("/messages/:id", function(req, res) {
+  var id = req.params.id;
+  if ( messages[id] ) {
+    res.send(messages[id]);
+  }
+  else {
+    res.send(404, "Not found");
+  }
+});
+
+
 app.post("/message", function(req, res) {
+  _id++;
   var message = mixin({
-    "timestamp": (new Date()).getTime()
+    "id": _id,
+    "created": (new Date()).getTime()
   }, req.body);
 
-  messages.push(message);
+  messages[_id] = message;
   res.send(message);
 });
 
 
-app.get("/messages", function(req, res) {
+app.put("/messages/:id", function(req, res) {
+  var id = req.params.id;
+  if ( messages[id] ) {
+    mixin(messages[id], {
+      "updated": (new Date()).getTime()
+    }, req.body);
+
+    res.send(messages[id]);
+  }
+  else {
+    res.send(404, "Not found");
+  }
+});
+
+
+app.delete("/messages", function(req, res) {
+  messages = {};
   res.send(messages);
+});
+
+
+app.delete("/messages:id", function(req, res) {
+  var id = req.params.id;
+  if ( messages[id] ) {
+    res.send(messages[id]);
+    delete messages[id];
+  }
+  else {
+    res.send(404, "Not found");
+  }
 });
 
 
